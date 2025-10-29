28bcbfb379b2b6014f3e4a3a104817f1949caf7b
Midhunlal-n <midhun-lal.n@capgemini.com>
2025-10-29 12:16:29 +0530

    third commit - ecomm extentrprt

--- src/test/java/runners/FailedRunner_ecomm.java
+++ src/test/java/runners/FailedRunner_ecomm.java
@@ -3,12 +3,12 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features", glue = { "stepDefinitions_ecomm" }, plugin = { "pretty",
+@CucumberOptions(features = "@target/failed_scenarios.txt", glue = { "stepDefinitions_ecomm" }, plugin = { "pretty",
 		"html:target/cucumber-reports/ecomm/html-report.html", "json:target/cucumber-reports/ecomm/json-report.json",
 		"junit:target/cucumber-reports/ecomm/junit-report.xml",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","rerun:target/failed_scenarios.txt" }, monochrome = true, tags = "@SuccessFlow or @InvalidCred")
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, monochrome = true)
 
-public class TestRunner_ecomm extends AbstractTestNGCucumberTests {
+public class FailedRunner_ecomm extends AbstractTestNGCucumberTests {
 	/*
 	 * @Override
 	 * 
--- src/test/java/stepDefinitions_ecomm/hooks.java
+++ src/test/java/stepDefinitions_ecomm/hooks.java
@@ -1,7 +1,15 @@
 package stepDefinitions_ecomm;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
 import utils.testSetup;
 
 public class hooks {
@@ -9,6 +17,17 @@ public class hooks {
 
 	public hooks(testSetup setup) {
 		this.driver = setup.driver; // Initialize WebDriver from testSetup
+	}
+	
+	@AfterStep
+	public void ErrorScreenshot(Scenario scenario) throws IOException{
+		if(scenario.isFailed())
+		{
+			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			byte[] content = FileUtils.readFileToByteArray(screenshot);
+			scenario.attach(content, "image/png", "Error Screenshot");
+			System.out.println("Scenario failed - Capturing screenshot");
+		}
 	}
 	
 	@After
